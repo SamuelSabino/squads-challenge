bb8da7eaab0f5294bb6c79f1bdec728738771d8e
Samuel <lima.samuel81@gmail.com>
2021-02-08 01:18:18 -0300

    chore: add connection function in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 
 import { router } from './routers'
+import { connectionDB } from './services/mongo.service'
 
 const app = express()
 
@@ -8,4 +9,10 @@ app.use(express.json())
 
 app.use('/v1/products', router)
 
-app.listen('8080', () => console.log('Server running on port 8080'))
+const startServer = async (): Promise<void> => {
+  await connectionDB()
+
+  app.listen('8080', () => console.log('Server running on port 8080'))
+}
+
+startServer()
