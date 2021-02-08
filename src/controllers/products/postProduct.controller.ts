8db48e4978351d3dc67935328f049635f7d4b689
Samuel <lima.samuel81@gmail.com>
2021-02-08 01:17:17 -0300

    refactor: add method of saving product

--- src/controllers/products/postProduct.controller.ts
+++ src/controllers/products/postProduct.controller.ts
@@ -1,7 +1,20 @@
 import { RequestHandler } from 'express'
 
-const postProduct: RequestHandler = (req, res) => {
-  res.json('')
-}
+import { Product } from '../../models/product.model'
 
+const postProduct: RequestHandler = (req, res) => {
+  try {
+    const product = new Product({
+      name: req.body.data.name,
+      description: req.body.data.description,
+      value: req.body.data.value
+    })
+
+    product.save()
+
+    res.status(200).json({ error: false, result: 'product successfully registered' })
+  } catch (error) {
+    res.status(500).json({ error: true, message: error })
+  }
+}
 export { postProduct }
