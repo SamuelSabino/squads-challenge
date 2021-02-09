bc0b59c7e838cb99e4d2a546758da583d2849ba4
Samuel <lima.samuel81@gmail.com>
2021-02-09 02:00:47 -0300

    feat: add mongo service

--- src/services/mongo.service.ts
+++ src/services/mongo.service.ts
@@ -1,16 +1,22 @@
 import mongoose from 'mongoose'
 
-export const connectionDB = async (): Promise<typeof mongoose> => {
-  return new Promise((resolve, reject) => {
-    const mongooseOptions = { useNewUrlParser: true, useUnifiedTopology: true }
+import { IMongoRepository } from '../interfaces/mongo.interface'
 
-    mongoose
-      .connect('mongodb://db:27017', mongooseOptions)
-      .then((result) => {
-        console.log('MongoDB Conectado')
+class MongoRepository implements IMongoRepository {
+  async connectionDB (): Promise<typeof mongoose> {
+    return new Promise((resolve, reject) => {
+      const mongooseOptions = { useNewUrlParser: true, useUnifiedTopology: true }
 
-        resolve(result)
-      })
-      .catch((error) => reject(error))
-  })
+      mongoose
+        .connect('mongodb://db:27017', mongooseOptions)
+        .then((result) => {
+          console.log('MongoDB Conectado')
+
+          resolve(result)
+        })
+        .catch((error) => reject(error))
+    })
+  }
 }
+
+export { MongoRepository }
