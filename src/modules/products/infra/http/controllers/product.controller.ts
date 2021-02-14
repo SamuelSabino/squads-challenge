6f51912e1f60c2aa28717b9d7a4c1ea5b08aad5f
Samuel <lima.samuel81@gmail.com>
2021-02-14 00:54:47 -0300

    refactor: improve the product module

--- src/modules/products/infra/http/controllers/product.controller.ts
+++ src/modules/products/infra/http/controllers/product.controller.ts
@@ -1,20 +1,27 @@
 import { Request, Response } from 'express'
-import { IProductDTO } from '../../../dtos/productDTO.interface'
 
+import { IProductDTO } from '../../../dtos/productDTO.interface'
+import IProductRepository from '../../../interfaces/product.interface'
+import { CreateProductService } from '../../../services/create-product.service'
+import { DeleteProductService } from '../../../services/delete-product.service'
+import { FindProductService } from '../../../services/find-product.service'
+import { UpdateProductService } from '../../../services/update-product.service'
 import { ProductRepository } from '../../mongo/repositories/product.repository'
 
 class ProductsController {
-  constructor (private productRepository = new ProductRepository()) { /**  */ }
+  constructor (private productRepository: IProductRepository = new ProductRepository()) { /**  */ }
 
   public async creatAndSave (req: Request, res: Response): Promise<void> {
     try {
       const product: IProductDTO = req.body
 
-      await this.productRepository.save(product)
+      const createProductService = new CreateProductService(this.productRepository)
 
-      res.status(200).json({ error: false, result: 'Product successfully created.' })
+      const productSaved = await createProductService.execute(product)
+
+      res.status(200).json({ error: false, result: productSaved, message: 'Product successfully created.' })
     } catch (error) {
-      res.json({ error: true, message: error })
+      res.json({ error: true, message: error.message })
     }
   }
 
@@ -22,25 +29,29 @@ class ProductsController {
     try {
       const id = req.params.id ?? ''
 
-      const product = (id)
-        ? this.productRepository.findById(id)
-        : this.productRepository.findAll()
+      const findProductService = new FindProductService(this.productRepository)
+
+      const product = await findProductService.execute(id)
 
       res.status(200).json({ error: false, result: product })
     } catch (error) {
-      res.json({ error: true, message: error })
+      res.json({ error: true, message: error.message })
     }
   }
 
   public async updateById (req: Request, res: Response): Promise<void> {
     try {
-      const id = req.params.id
+      const id = req.params.id ?? ''
 
-      const product = this.productRepository.findById(id)
+      const productData: IProductDTO = req.body
 
-      res.status(200).json({ error: false, result: product })
+      const updateProductService = new UpdateProductService(this.productRepository)
+
+      const productUpdated = await updateProductService.execute({ ...productData, id })
+
+      res.status(200).json({ error: false, result: productUpdated })
     } catch (error) {
-      res.json({ error: true, message: error })
+      res.json({ error: true, message: error.message })
     }
   }
 
@@ -48,11 +59,13 @@ class ProductsController {
     try {
       const id = req.params.id
 
-      this.productRepository.delete(id)
+      const deleteProductService = new DeleteProductService(this.productRepository)
+
+      await deleteProductService.execute(id)
 
       res.status(200).json({ error: false, result: 'Product successfully deleted' })
     } catch (error) {
-      res.json({ error: true, message: error })
+      res.json({ error: true, message: error.message })
     }
   }
 }
--- src/modules/products/infra/mongo/repositories/product.repository.ts
+++ src/modules/products/infra/mongo/repositories/product.repository.ts
@@ -5,19 +5,25 @@ import { Product } from '../models/product.model'
 class ProductRepository implements IProductRepository {
   constructor () { /** */ }
 
-  async save (product: IProductDTO): Promise<void> {
-    const newProduct = new Product({
-      name: product.name,
-      description: product.description,
-      value: product.value
-    })
+  async save (product: IProductDTO): Promise<IProductDTO> {
+    return new Promise((resolve, reject) => {
+      const newProduct = new Product({
+        name: product.name,
+        description: product.description,
+        value: product.value
+      })
 
-    newProduct.save()
+      newProduct.save((err, result) => {
+        (err)
+          ? reject(err)
+          : resolve(result as unknown as IProductDTO)
+      })
+    })
   }
 
   async findAll (): Promise<IProductDTO[]> {
     return new Promise((resolve, reject) => {
-      Product.find((err, products) => {
+      Product.find({ active: true }, (err, products) => {
         (err)
           ? reject(err)
           : resolve(products as unknown as IProductDTO[])
@@ -27,22 +33,23 @@ class ProductRepository implements IProductRepository {
 
   async findById (id: string): Promise<IProductDTO> {
     return new Promise((resolve, reject) => {
-      Product.findOne({ _id: id }, {}, {}, (err, result) => {
-        err
+      Product.findOne({ _id: id, active: true }, {}, {}, (err, result) => {
+        (err)
           ? reject(err)
           : resolve(result as unknown as IProductDTO)
       })
     })
   }
 
-  async update (id: string, product: IProductDTO): Promise<IProductDTO> {
+  async update (productData: IProductDTO & { id: string }): Promise<IProductDTO> {
     const productToUpdate = new Product({
-      name: product.name,
-      description: product.description,
-      value: product.value
+      name: productData.name,
+      description: productData.description,
+      value: productData.value
     })
+
     return new Promise((resolve, reject) => {
-      Product.updateOne({ _id: id }, productToUpdate, {}, (err, product) => {
+      Product.updateOne({ _id: productData.id, active: true }, productToUpdate, {}, (err, product) => {
         err
           ? reject(err)
           : resolve(product as unknown as IProductDTO)
@@ -50,11 +57,13 @@ class ProductRepository implements IProductRepository {
     })
   }
 
-  async delete (id: string): Promise<boolean> {
+  // TODO: validar retorno do delete
+  async delete (id: string): Promise<undefined> {
     return new Promise((resolve, reject) => {
       Product.updateOne({ _id: id }, { active: false }, null, (err, result) => {
-        reject(err)
-        resolve(!!result)
+        (err)
+          ? reject(err)
+          : resolve(result)
       })
     })
   }
